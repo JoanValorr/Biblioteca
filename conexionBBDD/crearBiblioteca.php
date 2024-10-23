c688c2b0854b20b92d96b1b3351c557796f495ea
Joan Valor <joanpvalor@gmail.com>
2024-10-23 19:26:43 +0200

    feat: Configuración para añadir biblioteca OK

--- conexionBBDD/crearBiblioteca.php
+++ conexionBBDD/crearBiblioteca.php
@@ -1,23 +1,30 @@
-
 <?php
+
 include_once('baseDatos.php');
 
 // Recupera los datos enviados por el formulario
 $name = $_POST['name'];
 $address = $_POST['address'];
 $phone = $_POST['phone'];
-
 // Asegúrate de que los campos no estén vacíos
-
 if (empty($name) || empty($address) || empty($phone)) {
-    echo "<br>Error: Todos los campos (name, address,phone) deben ser rellenados.";
+    echo "<br>Error: Todos los campos (name, address, phone) deben ser rellenados.";
     echo "<br><br><a href='index.php'>Volver</a>";
     return;
 }
+
+// Consulta para insertar los datos
 $query = "INSERT INTO library (name, address, phone) VALUES ('$name', '$address', '$phone')";
 $result = mysqli_query($conn, $query);
+
+
 if ($result) {
     header('Location: index.php');
 } else {
-    echo "Error al guardar el usuario: " . mysqli_error($conn);
+    echo "Error al guardar los datos: " . mysqli_error($conn);
+}
+
+// Cierra la conexión
+if (isset($conn)) {
+    mysqli_close($conn);
 }
