740ec05d22eae926a571a624bf462057a916f363
Joan Valor <joanpvalor@gmail.com>
2024-10-23 18:44:17 +0200

    feat: Cambios: Index.php y styles.css

--- conexionBBDD/index.php
+++ conexionBBDD/index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="UTF-8"></meta>
-        <link rel="stylesheet" href="/css/styles.css">
+        <link rel="stylesheet" href="../css/styles.css">
     </head>
 
 <?php
