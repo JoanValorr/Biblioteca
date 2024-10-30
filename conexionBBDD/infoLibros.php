45fae1099b9ef85dca71781b586adb3f23090d6e
Joan Valor <151775955+JoanValorr@users.noreply.github.com>
2024-10-30 11:54:45 +0100

    cambio y borrada de archivos

--- conexionBBDD/infoLibros.php
+++ conexionBBDD/infoLibros.php
@@ -1,44 +1,54 @@
-<html>
-    <head>
-        <meta charset="UTF-8"></meta>
-        <link rel="stylesheet" href="../css/styles.css">
-    </head>
-
 <?php
 
-
-include_once('baseDatos.php');
-
+include_once 'baseDatos.php';
 
 $consultaSelect = 'SELECT * FROM book';
 $resultadoSelect = mysqli_query($conn, $consultaSelect);
 
-echo '<table>';
-echo '<tr><th>ID</th>
-<th>Title</th><th>Author</th><th>ISBN</th><th>Language</th><th>Nombre Biblioteca</th></tr>';
-
-while ($row = mysqli_fetch_array($resultadoSelect)) {
-    $id = $row['id'];
-    echo '<tr>';
-    echo '<td>' . $row['id'] . '</td>';
-    echo '<td>' . $row['title'] . '</td>';
-    echo '<td>' . $row['author'] . '</td>';
-    echo '<td>' . $row['isbn'] . '</td>';
-    echo '<td>' . $row['language'] . '</td>';
-    echo '<td>' . $row['id_library'] . '</td>';
-    echo '<td>
-                <form action="detalleLibro.php" method="POST">
-                    <input type="hidden" name="id" value="' . $id . '">
-                    <input type="submit" value="Acceder">
-                </form>
-            </td>';
-    echo '</tr>';
-}
-
-echo '</table>'; // Cierra la tabla HTML.
-
-if (isset($conn)) {
-    mysqli_close($conn); // Cierra la conexión con la base de datos.
-}
-include_once 'creacionLibros.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Tabla para mostrar los datos -->
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+        </tr>
+
+        <?php while ($row = mysqli_fetch_array($resultadoSelect)): ?>
+            <?php $id = $row['id']; ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['author']; ?></td>
+                <td><?php echo $row['isbn']; ?></td>
+                <td><?php echo $row['language']; ?></td>
+                <td><?php echo $row['id_library']; ?></td>
+                <td>
+                    <form action="detalleLibro.php" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                        <input type="submit" value="Acceder">
+                    </form>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+
+        <?php
+        include_once 'creacionLibros.php';
+        ?>
+
+</body>
+
+</html>
