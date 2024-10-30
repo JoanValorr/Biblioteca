f32ce8cf64a5973d2e7a32d70973b9e8f1ea82e0
Guillem Murt <magicguillem@gmail.com>
2024-10-30 12:59:33 +0100

    feat:

--- conexionBBDD/crearBiblioteca.php
+++ conexionBBDD/crearBiblioteca.php
@@ -2,29 +2,33 @@
 
 include_once 'baseDatos.php';
 
-// Recupera los datos enviados por el formulario
 $name = $_POST['name'];
 $address = $_POST['address'];
 $phone = $_POST['phone'];
-// Asegúrate de que los campos no estén vacíos
-if (empty($name) || empty($address) || empty($phone)) {
-    echo "<br>Error: Todos los campos (name, address, phone) deben ser rellenados.";
-    echo "<br><br><a href='indexBibliotecas.php'>Volver</a>";
-    return;
-}
 
-// Consulta para insertar los datos
-$query = "INSERT INTO library (name, address, phone) VALUES ('$name', '$address', '$phone')";
-$result = mysqli_query($conn, $query);
+?>
 
 
-if ($result) {
-    header('Location: indexBibliotecas.php');
-} else {
-    echo "Error al guardar los datos: " . mysqli_error($conn);
-}
-
-// Cierra la conexión
-if (isset($conn)) {
-    mysqli_close($conn);
-}
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Bibliotecas</title>
+</head>
+<body>
+    <?php if (empty($name) || empty($address) || empty($phone)) : ?>
+        <h2>No has rellenado todos los campos.</h2>
+        <div class="backContainer">
+            <a href="indexBibliotecas.php">Volver</a>
+        </div>
+    <?php else : ?>
+        <?php
+            $queryInsert = 'INSERT INTO library (name, address, phone) VALUES ("' . $name . '", "' . $address . '", "' . $phone . '")';
+            $resultInsert = mysqli_query($conn, $queryInsert);
+            header('Location: indexBibliotecas.php');
+        ?>
+    <?php endif ?>
+</body>
+</html>
