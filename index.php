76e1bec8eee66eb1efeea48d7af00a7a6697f9d9
Joan Valor <151775955+JoanValorr@users.noreply.github.com>
2024-10-30 11:09:19 +0100

    Añadimos index nevo, y subimos para hacer pull desde el resto de ramas

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Página Principal</title>
+</head>
+<body>
+    <h1>Gestión Bibliotecas</h1>
+    <nav>
+        <ul>
+            <li><a href="peliculas/index.php">Ver Bibliotecas</a></li>
+            <li><a href="series/index.php">Ver Libros</a></li>
+        </ul>
+    </nav>
+    <!-- Formulario de Búsqueda -->
+    <form action="buscar.php" method="GET">
+        <label for="search">Buscar libro o Biblioteca:</label>
+        <input type="text" id="search" name="query" placeholder="Nombre o ID">
+        <button type="submit">Buscar</button>
+    </form>
+</body>
+</html>
